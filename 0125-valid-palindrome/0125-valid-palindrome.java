9b2f4d33730a950fe9eaadfccc5c5242e7892f37
Ashwin Mahajan <132732236+AshwinMahajan710@users.noreply.github.com>
2024-03-28 16:24:45 +0530

    Time: 2 ms (99%), Space: 43.1 MB (62.66%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,3 +1,4 @@
+// Better way check page no 2. array
 class Solution {
     public boolean isPalindrome(String s) {
         if (s.isEmpty()) {
@@ -23,3 +24,25 @@ class Solution {
         return true;
     }
 }
+
+ /*
+ The brutal way..
+
+ class Solution {
+    public boolean isPalindrome(String s) {
+        String new_str = "";
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length(); i++)
+            if ((s.charAt(i) >= 97 && s.charAt(i) <= 122) || (s.charAt(i) >= 48 && s.charAt(i) <= 57))
+                new_str += s.charAt(i);
+        int start = 0;
+        int end = new_str.length() - 1;
+        while (start < end) {
+            if (new_str.charAt(start) != new_str.charAt(end))
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+}*/ 
